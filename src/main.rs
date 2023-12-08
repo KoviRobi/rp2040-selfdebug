cc8ed2aebaa34a9242973b6351e4f6b81e409d55
Kovacsics Robert <kovirobi@gmail.com>
2023-12-08 00:47:34 +0000

    Add blining LED

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![no_std]
 #![no_main]
 
+use embedded_hal::digital::v2::OutputPin;
+
 use rp_pico as bsp;
 
 use bsp::entry;
-use bsp::hal::pac;
+use bsp::hal::prelude::*;
+use bsp::hal::{clocks::init_clocks_and_plls, pac, watchdog::Watchdog, Sio};
 
 use defmt::*;
 use defmt_rtt as _;
@@ -17,8 +20,38 @@ fn main() -> ! {
     println!("Hello, world!");
 
     let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let clocks = init_clocks_and_plls(
+        bsp::XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let sio = Sio::new(pac.SIO);
+    let pins = rp_pico::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
 
     cmsis_dap::dap_setup(&pac.SYSCFG.dbgforce);
 
-    loop {}
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let mut led_pin = pins.led.into_push_pull_output();
+
+    loop {
+        led_pin.set_high().unwrap();
+        delay.delay_ms(500);
+        led_pin.set_low().unwrap();
+        delay.delay_ms(500);
+        defmt::info!("tick");
+    }
 }
